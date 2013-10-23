7b3dae8d92b58cf5059cc088628cc6debb37513b
swdev <swdev.bali@gmail.com>
2013-10-23 09:35:05 +0700

    route

--- wsgi/main.py
+++ wsgi/main.py
@@ -2,5 +2,9 @@ from flask import Flask
 
 application = Flask(__name__)
 
+@application.route('/')
+def index():
+    return "Hello World!"
+
 if __name__ == '__main__':
     application.run(debug=True, host="0.0.0.0", port=8888)
