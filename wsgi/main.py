7fb3b8540e6e0f40f408507b671d5e5b583eaa66
swdev <swdev.bali@gmail.com>
2013-10-24 05:55:22 +0700

    render-template

--- wsgi/main.py
+++ wsgi/main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 application = Flask(__name__)
 
 @application.route('/')
 def index():
-    return "Hello World!"
+    return render_template('index.html', user = user)
 
 if __name__ == '__main__':
     application.run(debug=True, host="0.0.0.0", port=8888)
