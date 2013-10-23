b4eb93038c7676f394fe79bd88f766a8cac40e4e
swdev <swdev.bali@gmail.com>
2013-10-23 07:27:27 +0700

    package

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='Bio',
       version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      description="Let's you have a fully interactive Curriculum Vitae",
+      author='Eko S. Wibowo',
+      author_email='swdev.bali@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
+        'Flask==0.10.1', 
+        'MarkupSafe',
+        'Flask-SQLAlchemy==1.0',
+        'Flask-Login==0.2.7',
+        'Flask-WTF==0.9.2'
      )
