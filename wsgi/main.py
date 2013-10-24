7fa1d968c3a65ab3e8b9ae1b3e54d5dbb362401a
Eko Wibowo <swdev.bali@gmail.com>
2013-10-24 16:41:55 +0700

    fix

--- wsgi/main.py
+++ wsgi/main.py
@@ -4,7 +4,7 @@ application = Flask(__name__)
 
 @application.route('/')
 def index():
-    return render_template('index.html', user = user)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     application.run(debug=True, host="0.0.0.0", port=8888)
