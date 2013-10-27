3a9382ca543ecdc72325e76f45778ca9fce7205d
Eko Wibowo <swdev.bali@gmail.com>
2013-10-27 17:10:55 +0700

    bio layout

--- wsgi/main.py
+++ wsgi/main.py
@@ -2,9 +2,10 @@ from flask import Flask, render_template
 
 application = Flask(__name__)
 
-@application.route('/')
-def index():
-    return render_template('index.html')
+@application.route('/<username>')
+def index(username):
+    print username
+    return render_template('water/index.html')
 
 if __name__ == '__main__':
-    application.run(debug=True, host="0.0.0.0", port=8888)
+    application.run(debug=True, host="0.0.0.0", port=9999)
