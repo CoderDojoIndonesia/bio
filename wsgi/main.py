cd91e8dec9032f627be93e5aa4788222a2765881
Eko Wibowo <swdev.bali@gmail.com>
2013-10-28 14:16:19 +0700

    part iv

--- wsgi/main.py
+++ wsgi/main.py
@@ -2,10 +2,12 @@ from flask import Flask, render_template
 
 application = Flask(__name__)
 
+@application.route('/')
 @application.route('/<username>')
-def index(username):
-    print username
-    return render_template('water/index.html')
+def index(username = None):
+    if username is None:
+        return render_template('themes/water/index.html', page_title = 'Biography just for you!')
+    return render_template('themes/water/bio.html', page_title = username)
 
 if __name__ == '__main__':
     application.run(debug=True, host="0.0.0.0", port=9999)
