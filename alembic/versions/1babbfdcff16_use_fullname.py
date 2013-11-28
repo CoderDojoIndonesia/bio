5d7d9ace160c27a744e4920cb8cbcb3816937846
swdev <swdev.bali@gmail.com>
2013-11-28 09:50:43 +0700

    remove

--- alembic/versions/1babbfdcff16_use_fullname.py
+++ alembic/versions/1babbfdcff16_use_fullname.py
@@ -15,9 +15,21 @@ import sqlalchemy as sa
 
 
 def upgrade():
-    print "helo"
+    print "Adding fullname column"
     op.add_column('users', sa.Column('fullname', sa.String(101)))
+    
+    print "Merging firstname + lastname into fullname"
+    connection = op.get_bind()
+    connection.execute("update users set fullname = subquery.newfullname from (select id,concat(firstname, ' ', lastname) as newfullname from users) as subquery where users.id = subquery.id", execution_options = None)
+
+    print "Dropping firstname and lastname collumn"
+    op.drop_column('users', 'firstname')
+    op.drop_column('users', 'lastname')
+    
 
 
 def downgrade():
+    
+    connection = op.get_bind()
+    
     op.drop_column('users', 'fullname')
